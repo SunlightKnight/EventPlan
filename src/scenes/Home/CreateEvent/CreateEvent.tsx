8621187fbb7fc39642e57b28463b8b17cf6c2377
ElenaBertarelli <elena.bertarelli@mpolo.it>
2026-01-29 21:00:11 +0100

    aggiunta campi Create Event

--- src/scenes/Home/CreateEvent/CreateEvent.tsx
+++ src/scenes/Home/CreateEvent/CreateEvent.tsx
@@ -18,10 +18,13 @@ import { ScrollView } from "react-native-gesture-handler";
 import { icon_collapse, icon_expand } from "../../../assets/images";
 import DropShadow from "react-native-drop-shadow";
 import { AccountServiceContext } from "../../../Providers/Account/AccountServiceProvider";
-
+import { icon_school } from "../../../assets/images/index"
+import { PartecipantDTO } from "../../../models/services/PartecipantDTO";
+import { EventDTO } from "../../../models/services/EventDTO";
 
 
 type CreateEventProps = {
+  route: any;
   parentProps: any
 };
 
@@ -33,6 +36,7 @@ function CreateEvent(props: CreateEventProps) {
   const appContext = useContext(AppContext)
   const accountServiceContext = useContext(AccountServiceContext)
   const backendService = useContext(BackendServiceContext);
+  const { event } = props.route?.params
 
   const [userList, setUserList] = useState<Array<UserDTO>>([]);
   const [nameEvent, setNameEvent] = useState<string>("");
@@ -44,8 +48,9 @@ function CreateEvent(props: CreateEventProps) {
   const [text, onChangeText] = useState<string>('');
   const [categoryOpen, setCategoryOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("");
+  const accountContext = useContext(AccountServiceContext)
 
-let category = [
+  let category = [
     ('event_categories.undefined'),
     ('event_categories.school')
   ]
@@ -92,7 +97,7 @@ let category = [
   }
 
 
-  
+
 
   const createCategoryEntries = () => {
     if (category.length == undefined) {
@@ -105,20 +110,69 @@ let category = [
 
     for (let i = 0; i < (category.length); i++) {
       let view = (
-      <View style={styles.categoryEntry}>
-        <TouchableOpacity  onPress={() => {setSelectedCategory(category[i]); setCategoryOpen(false)}}>
-          <Text>
-            {t('' + category[i])}
-          </Text>
-        </TouchableOpacity>
-      </View>)
+        <View style={styles.categoryEntry}>
+          <TouchableOpacity onPress={() => { setSelectedCategory(category[i]); setCategoryOpen(false) }}>
+            <Text>
+              {t('' + category[i])}
+            </Text>
+          </TouchableOpacity>
+        </View>)
       cells.push(view)
     }
 
     return cells
   }
 
+  const isCurrentUserInParticipantsList = (event: EventDTO) => {
+      let participantsList: Array<PartecipantDTO> = event.partecipantiList
+      let i: number = 0
+  
+      for (i = 0; i < participantsList.length; i++) {
+        if (!(participantsList[i].username == accountContext?.aService.getUserName())) {
+          continue
+        }
+  
+        console.log(participantsList[i].idPartecipante, i)
+  
+        return [participantsList[i].idPartecipante, i]
+      }
+  
+      return [-100, -100]
+    }
+
+  const createParticipantEntries = (participants: Array<PartecipantDTO>) => {
+      if (participants == undefined) {
+        return <View>
+  
+        </View>
+      }
+  
+      let cells = new Array()
+      let i: number = 0
+  
+      for (i = 0; i < (participants.length); i++) {
+        cells[participants[i].idPartecipante] = <View style={styles.menuEntry}>
+          <Text style={styles.menuEntryName}>
+            {(participants[i].username == accountContext?.aService.getUserName() ? '> ' : '') + (participants[i].cognome ? participants[i].cognome : 'Doe') + " " + (participants[i].nome ? participants[i].nome : 'John')}
+          </Text>
+  
+          <Text style={styles.menuEntryEntry}>
+            {'Username: ' + (participants[i].username ? participants[i].username : 'N/A')}
+          </Text>
+          <Text style={styles.menuEntryEntry}>
+            {'Spesa: €' + (participants[i].spesa ? participants[i].spesa : '0')}
+          </Text>
+          <Text style={styles.menuEntryEntry}>
+            {'Data di pagamento: ' + (participants[i].dataPagamento ? participants[i].dataPagamento : 'NON EFFETTUATO')}
+          </Text>
+        </View>
+      }
+  
+      return cells
+    }
+
   //const categoryCells = createCategoryEntries();
+  const participantCells = createParticipantEntries(event.partecipantiList)
 
   return (
     <KeyboardAwareScrollView
@@ -147,15 +201,7 @@ let category = [
         value={text}
       />
 
-      {/* <Label
-        dimension="normal"
-        weight="semibold"
-        color={colors.mainText}
-        marginLeft={'5%'}
-        flex={5}
-        style={{}}>
-        {t("create.event_data")}
-      </Label> */}
+
       <DateTextField
         fieldTitle={t("create.event_data")}
         open={datePickerOpen}
@@ -170,29 +216,102 @@ let category = [
         onDatePickerPress={() => { setDatePickerOpen(true) }
         } />
 
+      <View style={styles.catContainer}>
+        <Label
+          dimension="normal"
+          weight="semibold"
+          color={colors.mainText}
+          marginLeft={'2%'}
+          flex={1}
+          style={{}}>
+          {t("create.event_category")}
+        </Label>
 
-      <DropShadow style={styles.generalShadow}>
-        <View style={styles.categoryContainer}>
-          <View style={styles.categoryHeader}>
-            <Text style={styles.categoryTitle}>
-              {t(selectedCategory)}
-            </Text>
-            <TouchableOpacity style={styles.categoryButtonHolder} onPress={() => { setCategoryOpen(!categoryOpen) }}>
-              <Image source={categoryOpen ? icon_collapse : icon_expand} style={styles.categoryButtonIcon} />
+        <View style={styles.columnContainer}>
+          <DropShadow style={styles.generalShadow}>
+            <View style={styles.categoryContainer}>
+              <TouchableOpacity style={styles.categoryHeader} onPress={() => { setCategoryOpen(!categoryOpen) }}>
+                <Text style={styles.categoryTitle}>
+                  {t(selectedCategory)}
+                </Text>
+                <View style={styles.categoryButtonHolder} >
+                  <Image source={categoryOpen ? icon_collapse : icon_expand} style={styles.categoryButtonIcon} />
 
-            </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+              {categoryOpen ? <View style={styles.categoryMainView}>
+                {createCategoryEntries()}
+              </View> : null}
+            </View>
+          </DropShadow>
+          <View>
+            <Image source={icon_school} style={styles.schoolIcon} />
+
           </View>
-          {categoryOpen ? <View style={styles.categoryMainView}>
-            {createCategoryEntries()}
-          </View> : null}
         </View>
-      </DropShadow>
+      </View>
+      <Label
+        dimension="normal"
+        weight="semibold"
+        color={colors.mainText}
+        marginLeft={'5%'}
+        style={{ marginLeft: padding.quarter }}>
+        {t("create.total_event")}
+      </Label>
+      <TextInput
+        onChangeText={(text) => setEventTotal(Number(text) || 0)}
+        keyboardType="numeric"
+      />
+
+      <Label
+        dimension="normal"
+        weight="semibold"
+        color={colors.mainText}
+        marginLeft={'5%'}
+        style={{ marginLeft: padding.quarter }}>
+        {t("create.desc_event")}
+      </Label>
+      <TextInput
+        style={styles.totalEvent}
+        onChangeText={setDescEvent}
+        value={text}
+      />
+      <View style={styles.columnContainer}>
+        <Label
+        dimension="normal"
+        weight="semibold"
+        color={colors.mainText}
+        marginLeft={'5%'}
+        style={{ marginLeft: padding.quarter }}>
+        {t("create._list_title")}
+      </Label>
+          <DropShadow style={styles.generalShadow}>
+            <View style={styles.categoryContainer}>
+              <TouchableOpacity style={styles.categoryHeader} onPress={() => { setCategoryOpen(!categoryOpen) }}>
+                <Text style={styles.categoryTitle}>
+                  {t(selectedCategory)}
+                </Text>
+                <View style={styles.categoryButtonHolder} >
+                  <Image source={categoryOpen ? icon_collapse : icon_expand} style={styles.categoryButtonIcon} />
+
+                </View>
+              </TouchableOpacity>
+              {categoryOpen ? <View style={styles.categoryMainView}>
+                {participantCells}
+              </View> : null}
+            </View>
+          </DropShadow>
+          
+        </View>
       <CustomButton
         text={t("home.create_event")}
         style={{ marginTop: padding.full }}
         onPress={() => {
           saveEvent()
         }} />
+
+        
+
     </KeyboardAwareScrollView>
   );
 
@@ -238,8 +357,8 @@ const styles = StyleSheet.create({
     shadowRadius: 6,
   },
   categoryContainer: {
-    marginHorizontal: 12,
-    marginVertical: 6,
+    marginHorizontal: '1%',
+    marginVertical: '2%',
     padding: 4,
     paddingHorizontal: 10,
     borderRadius: 6,
@@ -262,11 +381,50 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
 
   },
+
+  catContainer: {
+    marginHorizontal: '-0.5%',
+    marginVertical: '5%',
+    marginBottom: '5%',
+    padding: 10,
+    paddingHorizontal: 7,
+    borderRadius: 6,
+    flex: 3,
+
+    backgroundColor: colors.secondary,
+
+  },
   categoryEntry: {
     marginLeft: 8,
     fontSize: 20,
     color: colors.highlightText
-  }
+  },
+  schoolIcon: {
+    height: '75%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+    backgroundColor: colors.primary,
+    borderRadius: 6,
+  },
+
+  totalEvent:{
+    backgroundColor: colors.secondary,
+
+  },
+  menuEntry: {
+      marginVertical: 6
+    },
+    menuEntryName: {
+      marginLeft: 8,
+      fontSize: 20,
+      color: colors.highlightText
+    },
+    menuEntryEntry: {
+      marginLeft: 16,
+      fontSize: 16,
+      color: colors.mainText
+    },
+
 
 
 });
